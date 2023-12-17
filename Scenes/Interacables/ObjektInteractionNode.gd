904e30fe2ed969a8a1445d164fc31fd7242f1e2f
Elias Eggenberger <elias.eggenberger@hm.edu>
2023-12-17 22:03:21 +0100

    PopUpBox hinzugefügt

--- Scenes/Interacables/ObjektInteractionNode.gd
+++ Scenes/Interacables/ObjektInteractionNode.gd
@@ -6,17 +6,30 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$"../CanvasLayer/DialogueBox".start(textboxName) 
+	
+	
+		
+		
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	
+	pass 
 
 
 func _on_area_entered(area):
-	print("entered")
+	$Interaction.show()
+	if Input.is_action_pressed("ui_down"): 
+		$"../CanvasLayer/DialogueBox".start(textboxName)
+	
 	# Hier prüfen ob Player Klasse entered
 	$"../CanvasLayer/DialogueBox".start(textboxName)
 	pass # Replace with function body.
 
+
+
+func _on_area_exited(area):
+	$Interaction.hide()
+	pass # Replace with function body.
