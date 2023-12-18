37f2abec4ffd88963dccb86f68d4353d8c63ef64
Elias Eggenberger <elias.eggenberger@hm.edu>
2023-12-18 08:29:10 +0100

    Fehler behoben. Colllision Shape unabhängig vom popup resizen

--- Scenes/Interacables/ObjektInteractionNode.gd
+++ Scenes/Interacables/ObjektInteractionNode.gd
@@ -2,9 +2,11 @@ extends Area2D
 
 @export var textboxName ="" 
 @export var groupName  =""
+@onready var interaction = $"../Interaction"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	interaction.show() 
 	pass # Replace with function body.
 
 
@@ -16,7 +18,7 @@ func _process(delta):
 
 func _on_area_entered(area):
 	if area.is_in_group(groupName): 
-		$Interaction.show()
+		interaction.show() 
 		if Input.is_action_pressed("ui_down"): 
 			$"../CanvasLayer/DialogueBox".start(textboxName)
 	
@@ -25,5 +27,5 @@ func _on_area_entered(area):
 
 
 func _on_area_exited(area):
-	$Interaction.hide()
+	interaction.hide()
 	pass # Replace with function body.
