aa548de9f4ad1cb20d82b3b9880941b4f66ff890
Elias Eggenberger <elias.eggenberger@hm.edu>
2024-01-03 10:57:01 +0100

    Zimmer Szene fertig

--- Scenes/GameScenes/switching_scence.gd
+++ Scenes/GameScenes/switching_scence.gd
@@ -1,6 +1,10 @@
 extends Area2D
+@export var gamepath=""
+
+
+
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://Scenes/GameScenes/hallway_scene.tscn")
+		get_tree().change_scene_to_file(gamepath)
 		
