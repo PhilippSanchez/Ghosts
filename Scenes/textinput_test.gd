f71eb417cd988f4022a24d496694f9d518471599
Elias Eggenberger <elias.eggenberger@hm.edu>
2024-01-07 13:24:22 +0100

    Texteingabe

---
+++ Scenes/textinput_test.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+var right_option : bool 
+
+
+
+func _on_line_edit_text_submitted(new_text):
+	if new_text == "12" : 
+		print(true)
+	else: print(false) 
+	pass # Replace with function body.
